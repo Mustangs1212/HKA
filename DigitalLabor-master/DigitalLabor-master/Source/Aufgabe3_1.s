5e2b88f6939e146ddf7fd574603baa7b369b25b5
Mustangs1212 <enessercann2018@gmail.com>
2025-11-23 11:07:10 +0100

    tech

--- DigitalLabor-master/DigitalLabor-master/Source/Aufgabe3_1.s
+++ DigitalLabor-master/DigitalLabor-master/Source/Aufgabe3_1.s
@@ -3,16 +3,24 @@
  *
  * SoSe 2024
  *
- *  Created on: <$Date>
- *      Author: <$Name>
+ *  Created on: <$21.11.2025>
+ *      Author: <$Muhammed Enes Ercan>
  *
  *	Aufgabe : Unterprogrammaufruf
  */
 .text /* Specify that code goes in text segment */
 .code 32 /* Select ARM instruction set */
 .global main /* Specify global symbol */
+
+.equ DELAYCONST, 3
+
 main:
 
+loop_start:
+        bl delay
+        b loop_start
+
+delay:
 
 stop:
 	nop
